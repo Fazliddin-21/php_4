118a04de4c77114b19b04efbc367a697ca3ba425
Muhammadali <muhammadaliwebdeveloper@gmail.com>
2024-02-27 22:24:40 +0500

    audio crud

--- from.php
+++ from.php
@@ -1,5 +1,6 @@
 <?php
-if(!isset($_SESSION['data'])) {
+
+if (!isset($_SESSION['data'])) {
   $_SESSION['data'] = [];
 }
 
@@ -7,24 +8,41 @@ if(!isset($_SESSION['data'])) {
 if (isset($_POST['frist']) && isset($_POST['last']) && isset($_POST['email']) && isset($_POST['password']) && isset($_POST['texts']))
 
   if ($_POST['frist'] != '' && $_POST['last'] != '' && $_POST['email'] != '' && $_POST['password'] != '' && $_POST['texts'] != '') {
-   
+
     if (isset($_FILES['vedio'])) {
 
-    
+
       if ($_FILES['vedio']['size'] != 0) {
         $file = $_FILES['vedio'];
         $type = explode('/', $file['type'])[1];
         $vedio = 'videos/' . md5($file['name']) . '.' . $type;
-        $types = ['mp4','mpeg4'];
+        $types = ['mp4', 'mpeg4'];
         // echo $name;
-    
-    
+
+
         if (in_array($type, $types)) {
           move_uploaded_file($file['tmp_name'], $vedio);
-         
         }
       }
+    } else {
+      echo 'Video formati notogri: ' . $type;
+    }
 
+    if (isset($_FILES['audio'])) {
+
+      if ($_FILES['audio']['size'] != 0) {
+        $file = $_FILES['audio'];
+        $type = explode('/', $file['type'])[1];
+        $audio = 'audios/' . md5($file['name']) . '.' . $type;
+        $types = ['mp3', 'aac', 'mpeg'];
+
+        if (in_array($type, $types)) {
+
+          move_uploaded_file($file['tmp_name'], $audio);
+        } else {
+          echo 'Audio formati notogri: ' . $type;
+        }
+      }
     }
 
 
@@ -33,16 +51,16 @@ if (isset($_POST['frist']) && isset($_POST['last']) && isset($_POST['email']) &&
     if (isset($_FILES['rasm'])) {
 
 
-      print_r($_FILES['rasm']);
-    
+      // print_r($_FILES['rasm']);
+
       if ($_FILES['rasm']['size'] != 0) {
         $file = $_FILES['rasm'];
         $type = explode('/', $file['type'])[1];
         $name = 'imegas/' . md5($file['name']) . '.' . $type;
         $types = ['jpg', 'png', 'gif', 'jpeg'];
         // echo $name;
-    
-    
+
+
         if (in_array($type, $types)) {
           move_uploaded_file($file['tmp_name'], $name);
           $_SESSION['data'][] = [
@@ -52,17 +70,15 @@ if (isset($_POST['frist']) && isset($_POST['last']) && isset($_POST['email']) &&
             'password' => $_POST['password'],
             'texts' => $_POST['texts'],
             'rasm' => $name,
-            'vedio' =>$vedio
-          ]; 
+            'vedio' => $vedio,
+            'audio' => $audio
+          ];
           header('Location:index.php');
         }
+      } else {
+        echo 'Rasm formati notogri: ' . $type;
       }
-
     }
-
-   
-    
-    
   }
 
 ?>
@@ -72,35 +88,47 @@ if (isset($_POST['frist']) && isset($_POST['last']) && isset($_POST['email']) &&
 
 <form action="index.php" method="post" enctype="multipart/form-data">
   <div class="mb-3">
-    <label for="exampleFormControlInput1" class="form-label"> address</label>
+    <label for="exampleFormControlInput1" class="form-label">First name</label>
     <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Frist" name="frist">
   </div>
   <div class="mb-3">
-    <label for="exampleFormControlInput1" class="form-label"> address</label>
+    <label for="exampleFormControlInput1" class="form-label">Last name</label>
     <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="last" name="last">
   </div>
   <div class="mb-3">
-    <label for="exampleFormControlInput1" class="form-label"> address</label>
-    <input type="" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com" name="email">
+    <label for="exampleFormControlInput1" class="form-label">Country</label>
+    <input type="text" class="form-control " placeholder="Uzbekistan" name="email">
   </div>
+
   <div class="mb-3">
-    <div>
-     <input type="file" name="rasm">
-    </div>
+    <label for="exampleFormControlInput1" class="form-label">Province</label>
+    <input type="text" class="form-control" placeholder="password" name="password">
   </div>
-  <div class="mb-3">
-    <div>
-     <input type="file" name="vedio">
-    </div>
-  </div>
-  <div class="mb-3">
-    <label for="exampleFormControlInput1" class="form-label">password</label>
-    <input type="password" class="form-control" id="exampleFormControlInput1" placeholder="password" name="password">
-  </div>
+
+
   <div class="mb-3">
     <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="texts"></textarea>
-    <button type="Submit" class="btn btn-danger">Submit</button>
+    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="texts" placeholder="Textarea"></textarea>
   </div>
- 
+
+  <div class="mb-3 " style="display: flex;">
+    <div>
+      <input type="file" name="rasm" class="file-input rasm-input">
+    </div>
+
+    <div>
+      <input type="file" name="vedio" class="file-input video-input">
+    </div>
+
+    <div>
+      <input type="file" name="audio" class="file-input audio-input">
+    </div>
+
+  </div>
+
+
+
+  <button type="Submit" class="mt-3 btn btn-danger">Submit</button>
+
+
 </form>
--- table.php
+++ table.php
@@ -1,28 +1,36 @@
+<?php
+function ImageUrl($img)
+{
+  $host = $_SERVER['HTTP_HOST'];
+  $port = $_SERVER['SERVER_PORT'];
+  $path = dirname($_SERVER['PHP_SELF']);
+
+  $result = 'http://' . $host . ':' . $port . $path  . '/' . $img;
+  return $result;
+}
+
+?>
+
+
 <tbody>
-  <?php
-  function ImageUrl($img)
-  {
-      $host = $_SERVER['HTTP_HOST'];
-      $port = $_SERVER['SERVER_PORT'];
-      $path = dirname($_SERVER['PHP_SELF']);
-  
-      $result = 'http://'.$host . ':' . $port .$path  . '/' . $img;
-      return $result;
-  }
-  
-  ?>
+
   <?php
   foreach ($_SESSION['data'] as $key => $value) :
   ?>
-    <tr>]
-    <th scope="row"><?= $key ?></th>
+    <tr class="table-tr">
+      <th scope="row"><?= $key ?></th>
+
       <td>
         <div class="img" style="background-image: url( <?= ImageUrl($value['rasm']) ?> );"></div>
       </td>
+
       <td>
-        <video src="<?= ImageUrl($value['vedio'])?> " width="100px" controls></video>
+        <video class="img" src="<?= ImageUrl($value['vedio']) ?> " width="100px" controls></video>
       </td>
-      
+      <td>
+        <audio src="<?= ImageUrl($value['audio']) ?> " width="100px" controls></audio>
+      </td>
+
       <th><?= $value['frist'] ?></th>
       <th><?= $value['last'] ?></th>
       <th><?= $value['email'] ?></th>
@@ -33,4 +41,5 @@
   <?php
   endforeach;
   ?>
+
 </tbody>
